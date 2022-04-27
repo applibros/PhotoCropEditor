823ccf22c33950137fb51236ed271a1ecba16726
y.kanomata <y.kanomata.applibros@gmail.com>
2022-04-27 18:01:35 +0900

    Support Swift5.

--- CropViewController/ResizeControl.swift
+++ CropViewController/ResizeControl.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-protocol ResizeControlDelegate: class {
+protocol ResizeControlDelegate: AnyObject {
     func resizeControlDidBeginResizing(_ control: ResizeControl)
     func resizeControlDidResize(_ control: ResizeControl)
     func resizeControlDidEndResizing(_ control: ResizeControl)
@@ -38,7 +38,7 @@ class ResizeControl: UIView {
         addGestureRecognizer(gestureRecognizer)
     }
     
-    func handlePan(_ gestureRecognizer: UIPanGestureRecognizer) {
+    @objc func handlePan(_ gestureRecognizer: UIPanGestureRecognizer) {
         if !enabled {
             return
         }
